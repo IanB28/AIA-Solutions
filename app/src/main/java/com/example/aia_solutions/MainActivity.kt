9e39d5ec89dfe4b2e8072834bf00e843685108e9
AlejandroBR10 <alejabaldera@gmail.com>
2026-03-11 00:36:06 -0600

    Agregando Fragmentos y Menu Inferior

--- app/src/main/java/com/example/aia_solutions/MainActivity.kt
+++ app/src/main/java/com/example/aia_solutions/MainActivity.kt
@@ -5,17 +5,41 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-
+import androidx.fragment.app.Fragment
+import com.example.aia_solutions.InicioFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var bottomNavigation: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        bottomNavigation = findViewById(R.id.bottomNavigation)
+        if (savedInstanceState == null) {
+            cargarFragment(InicioFragment())
         }
+        bottomNavigation.setOnItemSelectedListener { item ->
+
+
+            val fragment: Fragment = when (item.itemId) {
+                R.id.nav_inicio -> InicioFragment()
+                R.id.nav_perfil -> PerfilFragment()
+                R.id.nav_configuracion -> ConfigFragment()
+                else -> InicioFragment()
+
+            }
+            cargarFragment(fragment)
+            true
+        }
+
     }
+
+
+    private fun cargarFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.contenedorFragmentos, fragment)
+            .commit()
+    }
+
 }
